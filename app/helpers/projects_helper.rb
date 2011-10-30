4264b7348bc9b6c7193f82348c7cc672541e9e78
Raphael Randschau <nicolai86@me.com>
2011-10-30 22:22:01 +0100

    remove notice links, add feedback_snippet helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,13 @@
 module ProjectsHelper
+  def feedback_snippet(project)
+    js = "
+      javascript:( function() {
+         var script = document.createElement('script');
+         script.type = 'text/javascript';
+         script.src = 'http://#{request.host}/assets/client.js?' + (new Date().getTime()) + '&project_id=#{project.id}';
+         document.getElementsByTagName('body')[0].appendChild(script);
+       })()
+    ".gsub(/\s/,'')
+    "<a href='#' click='#{js}'>Feedback</a>"
+  end
 end
