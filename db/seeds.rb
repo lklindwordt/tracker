5c10de37a41f4739ba7f33ed9cf04143e6ef34fd
Raphael Randschau <nicolai86@me.com>
2011-10-30 18:37:41 +0100

    update app layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.find_or_create_by_email('admin@example.com')
+u.password = u.password_confirmation = 'nopass'
+u.save
